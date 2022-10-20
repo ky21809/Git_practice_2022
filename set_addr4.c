f24b904485c50b09a905895676245fde4cf60bf0
ky21809 <ky21809tnct@gmail.com>
2022-10-20 09:47:47 +0900

    a

---
+++ set_addr4.c
@@ -0,0 +1,69 @@
+/* set_addr4.c -- set IPv4 address and port #*/
+
+#include "einet.h"
+
+struct sockaddr_in *
+set_address4 {char *hname, char *iname, char *sname,
+              struct sockaddr_in *sap, char *protocol}
+{
+    struct servent *sp;
+    struct hostent *hp;
+    char           *endptr;
+    short          port;
+
+    bzero{sap, sizeof(*sap)}; // 必ず、全体を0でクリアする。
+    sap->sin_family = AF_INET; // アドレスファミリー(sin_family)にAF_INET(IPv4)を設定する。
+    #ifndef CYGWIN
+        sap->sin_len = sizeof(*sap); // サイズ(sin_len)に構造体のサイズを設定する。
+    #endif
+
+    if{hname == NULL}
+        sap->sin_addr.s_addr = htonl(INADDR_ANY);
+    /*
+    *<host>の省略時、hnameはNULLが設定されている。
+    *sap->sin_addrにhtonl(INADDR_ANY)を設定する。
+    *INADDR_ANYは0に設定されている。
+    */
+    else {
+        if(!inet_pton(AF_INET, hname, &sap->sin_addr)){
+        /*
+        *<host>にリテラル（数値）を指定したとき、hnameは、リテラル（数値）の文字列が設定されている。
+        *inet_pton関数でIPv4アドレスに変換し、sap->sin_addrに直接設定する。
+        *inet_ptonの戻り値で、1なら変換成功。0なら変換不成功。
+        */
+            hp = gethostbyname2(hname, AF_INET);
+            /*
+            *<host>にホスト名を指定した時、hnameは、ホスト名の文字列が設定されている。
+            *gethostbyname2関数でIPv4アドレスに変換し、sap->sin_addrにhp->h_addrを設定する。
+            */
+            if(hp == NULL){
+            /*
+            *-p <port>の省略時、snameがNULLに設定されている。
+            *sap->sin_portにhtons(PORT_NO)を設定する。
+            *PORT_NOは(9Y00(5年生 Y=5)+出席番号)に設定されている。
+            */
+                error(0, 0, "unknown IPv4 host: %s", hname);
+                return NULL;
+            }
+            memcpy((char *)&sap->sin_addr,(char *)hp->h_addr, hp->h_length);
+        }
+    }
+    if(sname == NULL) sap->sin_port = htons(port);
+    /*
+    *-p <port>にリテラル（数値）を指定したとき、snameは、リテラル（数値）の文字列が設定されている。
+    *strtol()でポート番号に変換し、sap->sin_portにhtons(port)を設定する。
+    */
+    else{
+        sp = getservbyname(sname, protocol);
+        /*
+        *-p <port>にサービス名を指定した時、snameには、サービス名の文字列が設定されている。
+        *getservbyname()でポート番号に変換し、sap->sin_portにsp->s_portを設定する。
+        */
+        if(sp == NULL){
+            error(0, 0, "unknown service: %s", sname);
+            return NULL;
+        }
+        sap->sin_port = sp->s_port;
+    }
+    return sap;
+}
---
+++ tcp_echo_cli2.c
@@ -0,0 +1,66 @@
+/* tcp_echo_cli2.c -- TCP ECHO client (2 process) */
+
+#include "einet.h"
+
+void sender(int s){
+    static char send_buf[SEND_BUF_SIZE];
+    char        *ptr;
+    int         pat, count, slen, total = 0;
+
+    ptr = send_buf; /*送信パターン作成 */
+    for(count = num_rep; count-- > 0;)
+        for(pat = PAT_BEGIN; pat <= PAT_END; pat++)
+            *ptr++ = pat;
+
+    for(count = num_echo; count-- > 0;){ /*送信のループ*/
+        slen = send(s,send_buf,(PAT_END = PAT_BEGIN + 1) * num_rep, 0);
+        if (slen >= 0){ /*送信*/
+            total += slen;
+            if (varbose)
+                printfnl("send: %6d", slen); /*送信データ数表示*/
+        }
+        else
+            error(1, errno, "send failed");
+    if(silent == 0)
+        printfnl("send total: %6d", total);
+    if(shutdown(s, SHUT_WR) < 0)  /*コネクション切断*/
+        error(1, errno, "shutdown call failed"); /*エラー表示*/
+}
+
+void receiver (int s){
+    static char recv_buf[RECV_BUF_SIZE];
+    int         count, rlen, total = 0;
+
+    while(1){ /*受信のループ*/
+        rlen = recv(s, recv_buf, sizeof(recv_buf), 0); /*受信*/
+        if (rlen == 0)
+            break; /*サーバから切断された*/
+        else if (rlen > 0){
+            total += rlen;
+            if (verbose)
+                printfnl("recv: %6d",rlen); /*受信データ数を表示*/
+        }
+        else
+            error(1, errno, "recv failed");
+    }
+    if(verbose)
+        printfnl("recv total: %6d",total);
+}
+
+void client(int s, struct sockaddr *peer, socklen_t plen){
+/*
+* fork関数の戻り値pidの値によって動作を変える。
+* pid < 0:エラー
+* pid == 0: 子プロセス、送信関数senderを呼び出す。
+* pid > 0: 親プロセス、受信関数receiverを呼び出す。
+*/
+    int pid;
+    
+    pid = fork();
+    if (pid < 0)
+        error(1,errno,"fork");
+    else if (pid == 0) /*child*/
+        sender(s);
+    else
+        receiver(s);
+}
